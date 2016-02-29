89b2755da8b73d1dc7fdb6a9ed4863e22731ddf1
reandeer <reandeer_2000@hotmail.com>
2016-02-29 11:34:04 +0700

    create SQLigth finish

--- app/src/main/java/com/beenets/suwichai_s/bencharestaurant/MainActivity.java
+++ app/src/main/java/com/beenets/suwichai_s/bencharestaurant/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private MyManage myManage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Request Database
+        myManage = new MyManage(this);
+
+    } // Main method
+
+} // Main class
