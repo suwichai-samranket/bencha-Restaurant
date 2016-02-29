d9a9ba20ae9a5eb78750fa75187d3938ab54b7a9
reandeer <reandeer_2000@hotmail.com>
2016-02-29 10:14:36 +0700

    Start

---
+++ app/src/main/java/com/beenets/suwichai_s/bencharestaurant/MainActivity.java
@@ -0,0 +1,14 @@
+package com.beenets.suwichai_s.bencharestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+git s
